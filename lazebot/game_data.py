21525f47e42d1bf5d034560e18524975c92fe3b3
laze-b <lazebastra@gmail.com>
2024-01-15 20:55:45 -0600

    More reorgs

--- lazebot/game_data.py
+++ lazebot/game_data.py
@@ -1,6 +1,5 @@
 import requests
 import json
-import csv
 
 __useApi = False
 __cache = True
@@ -50,59 +49,6 @@ def fetch_players(guild):
         players.append(Player(allyCode, player["data"]["name"], units))
     return players
 
-
-def fetch_op_reqs():
-    with open("../lazebot/data/ops.txt") as f:
-        op_reqs = {}
-        reader = csv.DictReader(f, delimiter="\t")
-        for op in reader:
-            base_id = op["baseId"]
-            name = op["character_name"]
-            phase = op["phase"]
-            relic_tier = min(9, int(phase) + 4)
-            op_req = op_reqs.setdefault(base_id, OpReq(name))
-            op_req.add_req(relic_tier)
-        return op_reqs
-
-
-class OpReq:
-    def __init__(self, name, relic_tiers=None):
-        if relic_tiers is None:
-            relic_tiers = []
-        self.name = name
-        self.relic_tiers = relic_tiers
-
-    def add_req(self, relic_tier):
-        self.relic_tiers.append(relic_tier)
-
-    def total_units(self, max_relic_tier):
-        total_count = 0
-        matching = [rt for rt in self.relic_tiers if rt <= max_relic_tier]
-        for current_relic_tier in range(max_relic_tier, 4, -1):
-            # add the current tier to the count
-            current_count = matching.count(current_relic_tier)
-            total_count += current_count
-            # remove lower tiers to prevent duplicate counting
-            for lower_tier in range(current_relic_tier - 1, 4, -1):
-                for x in range(current_count):
-                    if lower_tier in matching:
-                        matching.remove(lower_tier)
-            matching = [rt for rt in matching if rt < current_relic_tier]
-
-        return total_count
-
-    def max_matching(self, relic_tier):
-        matching = [rt for rt in self.relic_tiers if rt <= relic_tier]
-        if matching:
-            return max(matching)
-        else:
-            return -1
-
-    def __str__(self):
-        return f'OpReq("{self.name}", {self.relic_tiers}'
-
-    def __repr__(self):
-        return f'OpReq("{self.name}", {self.relic_tiers}'
 
 
 class Player:
--- lazebot/guild_score.py
+++ lazebot/guild_score.py
@@ -23,21 +23,7 @@ def compute_guild_score(guild_id):
                 player_score.add_unit_score(guild_ops[unit.baseId], unit)
 
 
-class GuildOp:
-    def __init__(self, op_req):
-        self.opReq = op_req
-        self.guildUnits = []
 
-    def add_guild_unit(self, unit):
-        self.guildUnits.append(unit)
-        self.guildUnits.sort(reverse=True, key=GuildOp.sort_unit)
-
-    @staticmethod
-    def sort_unit(unit):
-        if unit.gearLevel == 13:
-            return unit.gearLevel + unit.relicTier
-        else:
-            return unit.gearLevel
 
 
 class PlayerScore:
